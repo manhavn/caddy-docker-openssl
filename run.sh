16ef677ed00ee6acd502f88133c58a091a318d25
manhbv <manhbv.giaminh@gmail.com>
2025-03-06 09:09:40 +0700

    add tls config

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+# brew install mkcert
+# mkcert -install
+# cd certs && mkcert devs-MacBook-Pro.local
+
 docker run --rm \
     -p 443:443 \
     -e IPV4PARRENT=$(ipconfig getifaddr en0) \
     -v ./caddyfile:/etc/caddy/Caddyfile \
+    -v ./certs:/etc/certs \
     -it caddy:latest
